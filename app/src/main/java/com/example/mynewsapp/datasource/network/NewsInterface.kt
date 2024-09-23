e01a13bbebf461b051cffcbae94d45435ce0c527
ar9988 <rladkfma9988@gmail.com>
2024-09-24 01:08:17 +0900

    Add Header

--- app/src/main/java/com/example/mynewsapp/datasource/network/NewsInterface.kt
+++ app/src/main/java/com/example/mynewsapp/datasource/network/NewsInterface.kt
@@ -3,6 +3,7 @@ package com.example.mynewsapp.datasource.network
 import com.example.mynewsapp.datasource.network.dto.News
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Query
 
 const val BASE_URL = "https://newsapi.org/"
@@ -10,10 +11,17 @@ const val API_KEY = "3b1a55634db24acdbb86d8eb9108dd7e"
 
 interface NewsInterface{
     @GET("v2/top-headlines?&apiKey=$API_KEY&pageSize=100")
-    fun getHeadLines(@Query("category") category: String,
-                     @Query("country") country: String,
-                     @Query("page") page : Int) : Call<News>
+    fun getHeadLines(
+        @Query("category") category: String,
+        @Query("country") country: String,
+        @Query("page") page: Int,
+        @Header("User-Agent") userAgent: String = "Mozilla/5.0"
+    ): Call<News>
     @GET("v2/everything?apiKey=$API_KEY")
-    fun getAllNews(@Query("q") q: String,@Query("page") page : Int) : Call<News>
+    fun getAllNews(
+        @Query("q") q: String,
+        @Query("page") page : Int,
+        @Header("User-Agent") userAgent: String = "Mozilla/5.0"
+        ) : Call<News>
 
 }
